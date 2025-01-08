7b30125fe1ad7bfe4de89e60ad5844a564cd2003
Elham <elhamwaheed777@gmail.com>
2025-01-09 04:06:54 +0500

    Create Album List and Song List

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "./component/Sidebar";
 import Player from "./component/Player";
+import Display from "./component/Display";
 
 function App() {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
